b6a4b16a825f56f71761c588632f78d01a9d2ee7
Gofran Alwaked <galwaked@fact.com.jo>
2024-10-08 15:05:36 +0300

    Initial Commit

---
+++ GofranCard(SwiftUI)/GofranCard_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  GofranCard_SwiftUI_App.swift
+//  GofranCard(SwiftUI)
+//
+//  Created by Gofran Alwaked on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GofranCard_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
